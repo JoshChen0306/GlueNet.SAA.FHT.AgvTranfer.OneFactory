61ad3569afce1d371b10e0aa4c7af9d5a0960ea9
Matt.Shen <Matt.Shen@saa-symtek.com>
2024-10-14 14:02:10 +0800

    2024/10/11  Matt:  協助睿明調整現場的ini。確認Dispatch可以將E區空Rack夾回B區放置。

--- Dispatch/svrPair/Properties/AssemblyInfo.cs
+++ Dispatch/svrPair/Properties/AssemblyInfo.cs
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // 您可以指定所有的值，也可以依照以下的方式，使用 '*' 將組建和修訂編號
 // 指定為預設值: 
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("2024.8.15.17")]
-[assembly: AssemblyFileVersion("2024.8.15.0")]
+[assembly: AssemblyVersion("2024.10.11.11")]
+[assembly: AssemblyFileVersion("2024.10.11.11")]
