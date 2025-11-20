4bf7fd2a7a273c81e6755e97343256377f85d250
Josh <wcome0306@gmail.com>
2025-11-20 18:55:32 +0800

    編譯後產生的內容

--- WebGui/SCP/obj/Debug/net6.0/SCP.AssemblyInfo.cs
+++ WebGui/SCP/obj/Debug/net6.0/SCP.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SCP")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a90672cce690b393d268672d78ead85976dc5185")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+587c83f6297ea8f1c3b873775a955cd602fb51f3")]
 [assembly: System.Reflection.AssemblyProductAttribute("SCP")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SCP")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
